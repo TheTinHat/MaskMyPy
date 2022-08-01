3e162697f6d541ae8d7cdb93056473e0baa3ea41
David Swanlund <david+git@swanlund.dev>
2022-07-31 21:24:36 -0700

    remove cli from package

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
         "scikit-learn>=1.1.1",
     ],
     python_requires=">=3.8",
-    entry_points={
-        "console_scripts": ["maskmypy=maskmypy.command_line:cli"],
-    },
+    # entry_points={
+    #     "console_scripts": ["maskmypy=maskmypy.command_line:cli"],
+    # },
 )
