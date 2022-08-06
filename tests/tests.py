a33a8e9a5886e66243cc97f5e8206fdcea33f403
David Swanlund <10473778+TheTinHat@users.noreply.github.com>
2022-08-05 17:23:15 -0700

    test: Parametrize tests to improve coverage

--- tests/tests.py
+++ tests/tests.py
@@ -1,10 +1,18 @@
 import geopandas as gpd
+import pytest
 from maskmypy import Donut, Donut_MaxK, Donut_Multiply, Street
+from numpy import random
 
 # Load base data
 points = gpd.read_file("tests/test_data/test_points.shp")
 populations = gpd.read_file("tests/test_data/test_population.shp")
 addresses = gpd.read_file("tests/test_data/test_addresses.shp")
+
+rng = random.default_rng(seed=12345)
+
+
+def gen_seeds(n):
+    return rng.integers(low=10000, high=100000, size=n)
 
 
 def basic_assertions(masking_class):
@@ -19,76 +27,88 @@ def basic_assertions(masking_class):
         ), "Sensitive and masked geometries intersect."
 
 
-def test_donut_random_xy():
-    modes = ["uniform", "areal"]
-    i = 100
-    for mode in modes:
-        for i in range(i):
-            DonutMasker = Donut(sensitive=points, distribution=mode)
-            offset_coords = DonutMasker._random_xy(1, 100)
-            assert isinstance(offset_coords[0], float), "Random XY offsets are not valid floats."
-            assert isinstance(offset_coords[1], float), "Random XY offsets are not valid floats."
-            assert (
-                offset_coords[0] > -100 and offset_coords[1] > -100
-            ), "Random XY offsets are outside input range."
-            assert (
-                offset_coords[0] < 100 and offset_coords[1] < 100
-            ), "Random XY offsets are outside input range."
+@pytest.mark.parametrize("distributions", ["uniform", "areal"])
+def test_donut_random_xy(distributions):
+    for i in range(100):
+        DonutMasker = Donut(sensitive=points, distribution=distributions)
+        offset_coords = DonutMasker._random_xy(1, 100)
+        assert isinstance(offset_coords[0], float), "Random XY offsets are not valid floats."
+        assert isinstance(offset_coords[1], float), "Random XY offsets are not valid floats."
+        assert (
+            offset_coords[0] > -100 and offset_coords[1] > -100
+        ), "Random XY offsets are outside input range."
+        assert (
+            offset_coords[0] < 100 and offset_coords[1] < 100
+        ), "Random XY offsets are outside input range."
 
 
 def test_seed_reproducibility():
-    i = 100
     numbers = []
-    for i in range(i):
-        DonutMasker = Donut(sensitive=points, seed=123456789)
+    for i in range(100):
+        DonutMasker = Donut(sensitive=points, seed=12345)
         offset_coords = DonutMasker._random_xy(1, 100)
         numbers.append(offset_coords)
     assert len(set(numbers)) == 1, "Random numbers are not deterministic given same seed."
 
 
 def test_seed_randomness():
-    i = 100
     numbers = []
-    for n in range(i):
-        DonutMasker = Donut(sensitive=points, seed=n)
+    for seed in gen_seeds(100):
+        DonutMasker = Donut(sensitive=points, seed=seed)
         offset_coords = DonutMasker._random_xy(1, 100)
         numbers.append(offset_coords)
-    assert len(set(numbers)) == i, "Random numbers are not unique across seeds."
+    assert len(set(numbers)) == 100, "Random numbers are not unique across seeds."
 
 
-def test_donut_mask_normal():
+@pytest.mark.parametrize("distributions", ["uniform", "gaussian", "areal"])
+@pytest.mark.parametrize("seeds", gen_seeds(5))
+def test_donut_mask_normal(distributions, seeds):
     DonutMasker = Donut(
         sensitive=points,
-        population=populations,
-        population_column="POP",
-        addresses=addresses,
-        seed=1235151512515,
-        distribution="gaussian",
+        seed=seeds,
+        distribution=distributions,
     )
     DonutMasker.execute()
     DonutMasker.displacement_distance()
     basic_assertions(DonutMasker)
 
 
-def test_donut_mask_max_k():
+@pytest.mark.parametrize("distributions", ["uniform", "gaussian", "areal"])
+@pytest.mark.parametrize("seeds", gen_seeds(5))
+def test_donut_mask_contained(distributions, seeds):
+    DonutMasker = Donut(
+        sensitive=points, container=populations, distribution=distributions, seed=seeds
+    )
+    DonutMasker.execute()
+    DonutMasker.displacement_distance()
+    basic_assertions(DonutMasker)
+    assert DonutMasker.masked["CONTAINED"].min() == 1, "Points were not contained."
+
+
+@pytest.mark.parametrize("distributions", ["uniform", "gaussian", "areal"])
+def test_donut_mask_max_k(distributions):
     DonutMasker = Donut_MaxK(
         sensitive=points,
         population=populations,
         population_column="POP",
+        distribution=distributions,
         addresses=addresses,
-        max_k_anonymity=100,
+        max_k_anonymity=20,
+        ratio=0.1,
     )
     DonutMasker.execute()
     DonutMasker.displacement_distance()
+    DonutMasker.k_anonymity_estimate()
     basic_assertions(DonutMasker)
 
 
-def test_donut_mask_pop_multiplier():
+@pytest.mark.parametrize("distributions", ["uniform", "gaussian", "areal"])
+def test_donut_mask_pop_multiplier(distributions):
     DonutMasker = Donut_Multiply(
         sensitive=points,
         population=populations,
         population_column="POP",
-        addresses=addresses,
+        distribution=distributions,
         population_multiplier=5,
         max_distance=100,
     )
@@ -106,6 +126,7 @@ def test_street_mask():
     )
     StreetMasker.execute()
     StreetMasker.displacement_distance()
+    StreetMasker.k_anonymity_actual()
     basic_assertions(StreetMasker)
 
 
