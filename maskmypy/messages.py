34119051c9217abdc012a0dc9486d85ef685cc08
David Swanlund <10473778+TheTinHat@users.noreply.github.com>
2023-02-26 19:12:03 -0800

    Add archive exists message

--- maskmypy/messages.py
+++ maskmypy/messages.py
@@ -1,5 +1,5 @@
 candidate_checksum_mismatch_msg = (
-    "Checksum does not match what is on record. Did the sensitive layer get edited?"
+    "Checksum does not match what is on record. Did a layer within the atlas get edited?"
 )
 
 multiple_atlases_detected_msg = (
@@ -11,3 +11,7 @@ candidate_crs_mismatch_msg = "CRS of candidate does not match atlas CRS."
 candidate_identical_to_sensitive_msg = "Candidate geometry is identical to sensitive geometry."
 
 multiple_container_intersection_msg = "Point intersects multiple polygons in container layer."
+
+archive_exists_msg = (
+    "Atlas already exists. Either delete the existing atlas, or set `overwrite=True`."
+)
