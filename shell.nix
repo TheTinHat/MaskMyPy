639f671e4b7928c9e61472c76494be412bc5b0ce
David Swanlund <10473778+TheTinHat@users.noreply.github.com>
2023-10-21 15:48:08 -0700

    Activate virtualenv in shell.nix

--- shell.nix
+++ shell.nix
@@ -5,6 +5,11 @@ pkgs.mkShell {
 
   buildInputs = [
     pkgs.python311
+    pkgs.python311Packages.pip
+    pkgs.python311Packages.virtualenv
   ];
   LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [ pkgs.stdenv.cc.cc ];
+  shellHook = ''
+    source env/bin/activate
+  '';
 }
