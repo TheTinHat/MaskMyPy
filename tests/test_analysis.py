85f15b24a8c044f4eb2763d6b9a1df529b068197
David Swanlund <10473778+TheTinHat@users.noreply.github.com>
2023-11-24 14:33:33 -0800

    Add analysis tests

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -1,28 +1,34 @@
+import os
+
 import geopandas as gpd
 import pytest
 
-from maskmypy import Atlas, analysis
+from maskmypy import analysis, donut
 
 
-@pytest.fixture
-def container():
-    return gpd.read_file("tests/boundary.geojson")
+def test_k_satisfaction(points, address):
+    masked = donut(points, 100, 500)
+    masked_k = analysis.k_anonymity(points, masked, address)
+    k_sat_1 = analysis.k_satisfaction(masked_k, 1)
+    k_sat_50 = analysis.k_satisfaction(masked_k, 50)
+    k_sat_999 = analysis.k_satisfaction(masked_k, 999)
+    assert k_sat_1 > 0.9
+    assert 0.1 < k_sat_50 < 0.9
+    assert k_sat_999 < 0.1
 
 
-def test_k_satisfaction():
-    pass
-
-
-def test_displacement():
-    pass
+def test_displacement(points):
+    masked_points = points.copy()
+    masked_points["geometry"] = masked_points.geometry.translate(50, 0, 0)
+    displacement = analysis.summarize_displacement(analysis.displacement(points, masked_points))
+    assert displacement["displacement_min"] == 50
+    assert displacement["displacement_max"] == 50
+    assert displacement["displacement_med"] == 50
+    assert displacement["displacement_mean"] == 50
 
 
 def test_estimate_k_address(points, address):
-    atlas = Atlas("test", in_memory=True)
-    atlas.add_sensitive(points)
-    atlas.donut(50, 500)
-    k = analysis._estimate_k(atlas.sensitive, atlas.get().mdf, atlas.population)
-    analysis.summarize_k(k)
+    pass
 
 
 def test_estimate_k_polygon():
@@ -36,32 +42,33 @@ def test_disaggregate():
 def test_mean_center_drift(points):
     masked_points = points.copy(deep=True)
     masked_points["geometry"] = masked_points.geometry.translate(50, 0, 0)
-    drift = analysis.drift(points, masked_points)
+    drift = analysis.central_drift(points, masked_points)
     assert drift == 50
 
 
-def test_ripleys_k(points):
-    atlas = Atlas("test", in_memory=True)
-    atlas.add_sensitive(points)
-    atlas.donut(10, 100)
-    distance_steps = 10
-    max_dist = analysis._ripleys_rot(atlas.sensitive)
-    min_dist = max_dist / distance_steps
-    kresult_sensitive = analysis.ripleys_k(
-        atlas.sensitive, max_dist=max_dist, min_dist=min_dist, steps=distance_steps
+def test_ripleys_k(points, tmpdir):
+    masked = donut(points, 100, 500)
+    kresult_sensitive = analysis.ripleys_k(points)
+    kresult_masked = analysis.ripleys_k(masked)
+
+    analysis.graph_ripleyresult(kresult_sensitive).savefig("SensitiveResult.png")
+    assert os.path.exists("SensitiveResult.png")
+    analysis.graph_ripleyresults(kresult_sensitive, kresult_masked, subtitle="Test Data").savefig(
+        "ComparisonResult.png"
     )
-    kresult_candidate = analysis.ripleys_k(
-        atlas.get(0).mdf, max_dist=max_dist, min_dist=min_dist, steps=distance_steps
-    )
-    analysis.graph_ripleyresult(kresult_sensitive)
-    analysis.graph_ripleyresults(
-        kresult_candidate, kresult_sensitive, subtitle=atlas.candidates[0].cid
-    )
+    assert os.path.exists("ComparisonResult.png")
 
 
-def test_nearest_neighbor_stats():
-    pass
+def test_nearest_neighbor_stats(points):
+    masked_points = points.copy()
+    masked_points["geometry"] = masked_points.geometry.translate(50, 0, 0)
+    nnd = analysis.nnd_delta(points, masked_points)
+    assert nnd["nnd_min_delta"] == 0
+    assert nnd["nnd_max_delta"] == 0
+    assert nnd["nnd_mean_delta"] == 0
 
-
-def test_compare_candidates():
-    pass
+    masked_points = donut(points, 100, 500)
+    nnd = analysis.nnd_delta(points, masked_points)
+    assert isinstance(nnd["nnd_min_delta"], float)
+    assert isinstance(nnd["nnd_max_delta"], float)
+    assert isinstance(nnd["nnd_mean_delta"], float)
